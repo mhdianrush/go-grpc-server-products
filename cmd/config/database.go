d9d9be1309448e8e9704dd3b17b27d4415d2b1fa
mhdianrush <mhdianrush@gmail.com>
2023-07-17 12:08:30 +0700

    Opened Connection to MySQL Database With Gorm

--- cmd/config/database.go
+++ cmd/config/database.go
@@ -1,2 +1,21 @@
 package config
 
+import (
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var logger = logrus.New()
+
+func ConnectDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:admin@tcp(127.0.0.1:3306)/go_gRPC_server_products?parseTime=true"), &gorm.Config{})
+	if err != nil {
+		logger.Println("Failed to Connect Database")
+	}
+	db.AutoMigrate()
+
+	logger.Println("Database Connected")
+
+	return db
+}
