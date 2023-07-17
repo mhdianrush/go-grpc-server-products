04b94bedbb4f6f0ac123f74a2937b9b6551b2ee9
mhdianrush <mhdianrush@gmail.com>
2023-07-17 09:37:17 +0700

    Developed for Setting Up a Server

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net"
 	"os"
 
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -14,5 +16,17 @@ func main() {
 		logger.Println(err.Error())
 	}
 	logger.SetOutput(file)
-	logger.Println("Running")
+
+	logger.Println("Server Running on Port 8080")
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		logger.Println(err.Error())
+	}
+
+	grpcServer := grpc.NewServer()
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		logger.Println(err.Error())
+	}
 }
