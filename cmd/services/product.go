5987a5fbb414b5918b0b09207fa03ee3462750ff
mhdianrush <mhdianrush@gmail.com>
2023-07-17 10:41:01 +0700

    Developed for GetPRoducts Function

--- cmd/services/product.go
+++ cmd/services/product.go
@@ -1,1 +1,45 @@
 package services
+
+import (
+	"context"
+	paginationpb "go-gRPC-server-products/pb/pagination"
+	productpb "go-gRPC-server-products/pb/product"
+)
+
+type ProductService struct {
+	productpb.UnimplementedProductServiceServer
+}
+
+func (p *ProductService) GetProducts(context.Context, *productpb.Empty) (*productpb.Products, error) {
+	products := &productpb.Products{
+		Pagination: &paginationpb.Pagination{
+			Total:       10,
+			PerPage:     5,
+			CurrentPage: 1,
+			LastPage:    2,
+		},
+		Data: []*productpb.Product{
+			{
+				Id:    1,
+				Name:  "Fortuner",
+				Price: 500000000,
+				Stock: 50,
+				Category: &productpb.Category{
+					Id:   1,
+					Name: "Car",
+				},
+			},
+			{
+				Id:    2,
+				Name:  "Nmax",
+				Price: 20000000,
+				Stock: 100,
+				Category: &productpb.Category{
+					Id:   2,
+					Name: "Motor Cycle",
+				},
+			},
+		},
+	}
+	return products, nil
+}
