74116ffbba48ffb15d79ab1bf612386e9a31ded2
mhdianrush <mhdianrush@gmail.com>
2023-07-30 23:06:39 +0700

    Developed for Using godotenv Package to Read env File that Used in Open Connection to MySQL Database

--- cmd/config/database.go
+++ cmd/config/database.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,12 +13,20 @@ import (
 var logger = logrus.New()
 
 func ConnectDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open("root:admin@tcp(127.0.0.1:3306)/go_gRPC_server_products?parseTime=true"), &gorm.Config{})
+	if err := godotenv.Load(); err != nil {
+		logger.Printf("failed load env file %s", err.Error())
+	}
+	
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME"),
+	)
+	db, err := gorm.Open(mysql.Open(dataSourceName), &gorm.Config{})
 	if err != nil {
-		logger.Println("Failed to Connect Database")
+		logger.Printf("failed connect to database %s", err.Error())
 	}
 
-	logger.Println("Database Connected")
+	logger.Println("database connected")
 
 	return db
 }
